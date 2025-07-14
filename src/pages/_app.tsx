99a6babc7c08e3d07510fb297882a2f430137970
밍 <sucresucces@icloud.com>
2025-07-14 19:51:56 +0900

    Page Router
    ✔ Page Router
    - Next.js에서 기본으로 제공하는 라우팅 방식
    - React Router처럼 라우팅을 위한 별도 설정 없이, pages 폴더 내 파일/폴더 구조만으로 자동으로 라우팅
    - 현재 많은 기업에서 사용하는 안정적인 방식
    
    ✔ Pages
    - pages 폴더 아래에 있는 파일 이름이 URL경로가 됨
    - /pages/index.tsx -> /
    - /pages/about.tsx -> /about
    - /pages/blog/post.tsx -> /blog/post
    
    - 폴더명을 이용한 중첩 라우팅도 가능
    - /pages/blog/index.tsx -> /blog
    
    - 파일이 없고 폴더만 있을 경우, 그 폴더 안의 index.tsx를 자동으로 매칭함
    
    ✔ 동적 경로(Dynamic Routes)
    - 대괄호를 사용하여 URL 경로의 일부를 변수처럼 처리할 수 있음
    // 파일명: pages/post/[id].tsx
    // 경로 예시: /post/1, /post/hello
    export default function Post({ params }) {
      return <div>Post ID: {params.id}</div>;
    }
    
    ✔ _app.tsx
    - 앱 전체에 공통적으로 적용되는 레이아웃 구성 가능
    - 모든 페이지를 감싸는 최상위 컴포넌트
    - Component : 현재 렌더링할 페이지 컴포넌트
    - pageProps : 해당 페이지에서 넘긴 props
    - 예시처럼 헤더, 글로벌 스타일, 공통 폰트 등을 여기에 넣음
    
    _document.tsx
    - HTML문서의 기본 구조를 커스터마이징할 때 사용
    - 일반적인 React에서는 직접 다루기 어려운 부분들 (예: <html>, <body>, 초기 렌더링 시 스타일 적용 등)을 설정 가능

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,11 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // Component : page 역할 / pageProps : 각 페이지에서 사용하는 props
+  return (
+    <>
+      <header>글로벌헤더</header>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  // 모든 페이지에 공통적으로 적용되는 HTML 구조를 정의
+  // 모든 페이지에 적용되어야 하는 메타 태그, 스타일, 캐릭터 셋 등을 설정
   return (
-    <Html lang="en">
+    <Html lang="kr">
       <Head />
       <body>
         <Main />
