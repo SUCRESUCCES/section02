b05ce62e2472eec5ff3de87bca52ec141621a868
밍 <sucresucces@icloud.com>
2025-07-14 23:32:47 +0900

    프리페칭
    Pre-Fetching
    Pre : 사전에,미리
    Fetching : 불러온다
    
    프리페칭
    - 특정 페이지로 사용자가 이동하기 전에, 해당 페이지의 리소스를 미리 불러오는 기능
    - 목적 : 사용자가 클릭했을 때 페이지 로딩 속도를 높이기 위함
    
    ✔ 프리페칭의 특징
    - 기본 동작 : Link 컴포넌트는 자동으로 해당 경로를 프리페칭함
    - 수동 제어 : <Link href="/..." prefetch={false}>를 사용하면 프리페칭을 끌 수 있음
    - 동적 이동 : router.push()를 사용하는 경우에는 자동 프리페칭이 적용되지 않음 -> 직접 router.prefetch()필요
    - 실행 시점 : 보통 컴포넌트가 마운트 될 때 실행함(useEffect 안에서)
    
    ✔ 코드 예시 요약
    useEffect(() => {
      router.prefetch("/test");
    }, []);
    - 페이지가 처음 로드될 때 /test 경로를 미리 불러옴
    - 이후 router.push("/test")를 실행하면 더 빠르게 이동
    
    ✔ 요약정리
    - Link : 자동 프리페칭(비활성화 가능)
    - router.push() : 수동 프리페칭 필요(router.prefetch())
    - 프리페칭은 항상 UX 향상( 빠른 페이지 전환)을 위한 Next.js의 최적화 기능

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   // Component : page 역할 / pageProps : 각 페이지에서 사용하는 props
@@ -11,16 +12,27 @@ export default function App({ Component, pageProps }: AppProps) {
   const onClickButton = () => {
     // 프로그래매틱한 페이지 이동
     router.push("/test");
-    // push() : 현재 페이지를 스택에 추가하고 새로운 페이지로 이동
-    // replace(): 현재 페이지를 스택에 추가하지 않고 새로운 페이지로 이동
-    // back():  이전 페이지로 이동
   };
+
+  {
+    /* Link는 프리페칭이 되었지만 onClick은 프리페칭이 안됨 */
+  }
+
+  useEffect(() => {
+    router.prefetch("/test");
+    // 페이지가 마운트 될 때 /test 페이지를 미리 불러온다
+    // 프리페칭은 빠른 페이지 이동을 위해 제공되는 기능임
+    // 프리페칭은 페이지가 마운트 될 때, 사전에 해당 페이지를 불러오는 기능
+  }, []);
   return (
     <>
       <header>
         <Link href={"/"}>index</Link>
         &nbsp;
-        <Link href={"/search"}>search</Link>
+        {/* 프리페칭 강제로 해지하는 방법 */}
+        <Link href={"/search"} prefetch={false}>
+          search
+        </Link>
         &nbsp;
         <Link href={"/book/1"}>book/1</Link>
         <div>
