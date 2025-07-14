fef0afcbaba40143eb55c89df60925262d47c8c4
밍 <sucresucces@icloud.com>
2025-07-14 22:16:48 +0900

    네비게이팅
    1. 사용자 클릭으로 이동(Link 컴포넌트)
    <Link href={"/"}>index</Link>
    <Link href={"/search"}>search</Link>
    <Link href={"/book/1"}>book/1</Link>
    - <Link>를 사용하면 페이지 간 클라이언트 사이드 라우팅이 이루어짐
    - 페이지 전체를 새로 고치지 않고, 빠르게 이동 가능
    
    2. 프로그래매틱 이동
    const router = useRouter();
    
    const onClickButton = () => {
      router.push("/test");
    };
    - 버튼 클릭 등 이벤트로 라우팅할 때 사용
    - router.push("/test")
    -> 현재 URL을 스택에 저장하고 /test 페이지로 이동
    
    - replace("/test"): 현재 페이지를 덮어씌움 (뒤로가기 시 원래 페이지로 돌아갈 수 없음)
    - back(): 이전 페이지로 이동

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,32 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
   // Component : page 역할 / pageProps : 각 페이지에서 사용하는 props
+
+  const router = useRouter();
+
+  const onClickButton = () => {
+    // 프로그래매틱한 페이지 이동
+    router.push("/test");
+    // push() : 현재 페이지를 스택에 추가하고 새로운 페이지로 이동
+    // replace(): 현재 페이지를 스택에 추가하지 않고 새로운 페이지로 이동
+    // back():  이전 페이지로 이동
+  };
   return (
     <>
-      <header>글로벌헤더</header>
+      <header>
+        <Link href={"/"}>index</Link>
+        &nbsp;
+        <Link href={"/search"}>search</Link>
+        &nbsp;
+        <Link href={"/book/1"}>book/1</Link>
+        <div>
+          <button onClick={onClickButton}>/test 페이지로 이동</button>
+        </div>
+      </header>
       <Component {...pageProps} />
     </>
   );
