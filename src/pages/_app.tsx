7ab60efa57900079a850f0498345c7bef94745dc
밍 <sucresucces@icloud.com>
2025-07-15 17:24:23 +0900

    글로벌 레이아웃 설정하기
    글로벌 레이아웃 컴포넌트 구성
    - 레이아웃 파일 : global-layout.module.tsx
    - 주요 구조
    <div className={style.container}>
      <header className={style.header}>...</header>
      <main className={style.main}>{children}</main>
      <footer className={style.footer}>...</footer>
    </div>
    - 각 영역에 CSS Module 스타일 적용 : global-layout.module.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,45 +1,11 @@
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  // Component : page 역할 / pageProps : 각 페이지에서 사용하는 props
-
-  const router = useRouter();
-
-  const onClickButton = () => {
-    // 프로그래매틱한 페이지 이동
-    router.push("/test");
-  };
-
-  {
-    /* Link는 프리페칭이 되었지만 onClick은 프리페칭이 안됨 */
-  }
-
-  useEffect(() => {
-    router.prefetch("/test");
-    // 페이지가 마운트 될 때 /test 페이지를 미리 불러온다
-    // 프리페칭은 빠른 페이지 이동을 위해 제공되는 기능임
-    // 프리페칭은 페이지가 마운트 될 때, 사전에 해당 페이지를 불러오는 기능
-  }, []);
   return (
-    <>
-      <header>
-        <Link href={"/"}>index</Link>
-        &nbsp;
-        {/* 프리페칭 강제로 해지하는 방법 */}
-        <Link href={"/search"} prefetch={false}>
-          search
-        </Link>
-        &nbsp;
-        <Link href={"/book/1"}>book/1</Link>
-        <div>
-          <button onClick={onClickButton}>/test 페이지로 이동</button>
-        </div>
-      </header>
+    <GlobalLayout>
       <Component {...pageProps} />
-    </>
+    </GlobalLayout>
   );
 }
