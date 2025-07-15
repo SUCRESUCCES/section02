1f97d5496f410500fca70772b8428973fc38108d
밍 <sucresucces@icloud.com>
2025-07-15 16:55:33 +0900

    스타일링
    CSS Module 사용
    1. CSS Module import
    import styles from './파일명.module.css'
    - 클래스명을 객체 형태로 사용 -> styles. 클래스명
    2. 스타일 적용
    <h1 className={styles.h1}>제목</h1>
    
    - 글로벌 CSS(reset. css 등)는 pages/_api.js 또는 pa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
+// CSS Module
+
+import style from "./index.module.css";
+
+// Global CSS cannot be imported from files other than your Custom <App>
+// 글로벌 CSS 파일은 App 컴포넌트가 아닌 곳에서는 불러올 수 없다
+
 export default function Home() {
-  return <h1>인덱스</h1>;
+  return (
+    <>
+      <h1 className={style.h1}>인덱스</h1>
+      <h2 className={style.h2}>H2</h2>
+    </>
+  );
 }
