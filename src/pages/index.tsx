63fd12209f84010ede957490d28b61b3274ee846
밍 <sucresucces@icloud.com>
2025-07-16 16:52:41 +0900

    한입북스  UI 구현하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,30 @@
 // CSS Module
 import { ReactNode } from "react";
 import style from "./index.module.css";
-
 import SearchableLayout from "@/components/searchable-layout";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
+// @ : src
 
 // Global CSS cannot be imported from files other than your Custom <App>
 // 글로벌 CSS 파일은 App 컴포넌트가 아닌 곳에서는 불러올 수 없다
 
 export default function Home() {
   return (
-    <>
-      <h1 className={style.h1}>인덱스</h1>
-      <h2 className={style.h2}>H2</h2>
-    </>
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
   );
 }
 
