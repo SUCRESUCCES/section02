e5c0403daaf8ae51ac2520f8b899aeb5821a7532
밍 <sucresucces@icloud.com>
2025-07-24 21:26:48 +0900

    SSG(정적 사이트 생성) 소개
    정적 사이트 생성
    SSG - Static Site Generation
    - SSR의 단점을 해결하는 사전 렌더링 방식
    - 빌드 타임에 페이지를 미리 사전렌더링 해 둠
    
    SSG의 장점
    - 사전 렌더링에 많은 시간이 소요되는 페이지더라도 사용자의 요청에는 매우 빠른속도로 응답 가능
    
    SSG 정적 경로에 적용하기
    
    ❕ SSG(getStaticProps) 정리
    - getStaticProps는 빌드 타임에 데이터를 미리 받아서 HTML을 생성해두는 방식
    - 사용자는 정적 HTML을 즉시 받기 때문에 빠름
    - 페이지에 사전 렌더링이 필요할 때 유용
    - 예시 : Home 페이지에서 getStaticProps를 사용해 fetchBooks()와 fetchRandomBooks()를 실행하고 그 결과를 props로 전달
    export const getStaticProps = async () => {
      const [allBooks, recoBooks] = await Promise.all([
        fetchBooks(),
        fetchRandomBooks(),
      ]);
      return {
        props: { allBooks, recoBooks },
      };
    };
    - 이렇게 하면 사용자가 접근했을 때 바로 이 데이터를 보여줄수 있어 빠름!
    
    ❕ CSR(클라이언트 측 데이터 요청 + useEffect)
    - getStaticProps는 쿼리스트링같은 동적 데이터에는 사용불가
    - 그래서 Page 컴포넌트에서는 router.query.q를 사용해서 클라이언트에서 검색어를 가져와서 fetchBooks(q)로 검색 결과 불러옴
    - 이건 useEffect로 동작하기 때문에 브라우저가 페이지 로딩한 후에 실행됨
    useEffect(() => {
      if (q) {
        fetchSearchResult(); // q가 있을 때만 실행
      }
    }, [q]);
    - 즉, 처음 페이지 로드 시엔 빈 상태였다가, 검색어가 감지되면 비동기로 데이터를 불러옴
    
    ○  (Static)   prerendered as static content
    ●  (SSG)      prerendered as static HTML (uses getStaticProps)
    ƒ  (Dynamic)  server-rendered on demand

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,23 @@ import { ReactNode } from "react";
 import style from "./index.module.css";
 import SearchableLayout from "@/components/searchable-layout";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType } from "next";
+// import { InferGetServerSidePropsType } from "next";
+import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export const getServerSideProps = async () => {
+// getServerSideProps : SSR 방식
+// getStaticProps : SSG 방식
+
+export const getStaticProps = async () => {
   // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
+
+  console.log("인덱스 페이지");
+
   // 직렬방식
   // const allBooks = await fetchBooks();
   // const recoBooks = await fetchRandomBooks();
+
   // 병렬방식(좀 더 빠름)
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
@@ -25,7 +33,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,25 +1,46 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { ReactNode } from "react";
+import { ReactNode, use, useEffect, useState } from "react";
 import BookItem from "@/components/book-item";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+  GetStaticPropsContext,
+} from "next";
+
 import fetchBooks from "@/lib/fetch-books";
+import { useRouter } from "next/router";
+import { BookData } from "@/type";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  // console.log(context);
-  const q = context.query.q;
-  const books = await fetchBooks(q as string);
-  return {
-    props: {
-      books,
-    },
+// 사전렌더링 과정에서는 쿼리스트링을 불러올 수 없다
+
+// export const getStatciProps = async (context: GetStaticPropsContext) => {
+//   // console.log(context);
+//   const q = context.query.q;
+//   const books = await fetchBooks(q as string);
+//   return {
+//     props: {
+//       books,
+//     },
+//   };
+// };
+
+export default function Page() {
+  const [books, setBooks] = useState<BookData[]>([]);
+
+  const router = useRouter();
+  const q = router.query.q;
+
+  const fetchSearchResult = async () => {
+    const data = await fetchBooks(q as string);
+    setBooks(data);
   };
-};
 
-export default function Page({
-  books,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  useEffect(() => {
+    if (q) {
+      // 검색 결과를 불러오는 로직
+      fetchSearchResult();
+    }
+  }, [q]);
   return (
     <div>
       {books.map((book) => (
