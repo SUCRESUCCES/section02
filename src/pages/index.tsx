ad6de53049aaf97872e4cf70de9b23c64c37e56c
밍 <sucresucces@icloud.com>
2025-07-24 15:50:25 +0900

    2.11) 소개 및 실습 복습

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 
 export const getServerSideProps = async () => {
+  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
   // 직렬방식
   // const allBooks = await fetchBooks();
   // const recoBooks = await fetchRandomBooks();
