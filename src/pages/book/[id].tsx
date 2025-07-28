d20526941a5c90039c132d94ae3ef7c6fb937a8f
밍 <sucresucces@icloud.com>
2025-07-28 20:01:27 +0900

    SSG 실습 - Fallback 옵션
    ✔ getStaticPaths의 fallback 옵션
    - getStaticPaths를 사용하는 동적 SSG 페이지에서 존재하지 않는 경로에 접근했을 때 동작을 결정하는 옵션
    
    fallback 종류별 요약
    - false : ❌ 미리 정의된 경로만 허용 -> 나머지는 404 페이지
    - blocking : ⏳ 해당 경로로 접속 시 서버에서 즉시 생성 후 보여줌(SSR처럼 작동)
    - true : ⏩ 먼저 빈 페이지 또는 로딩 상태로 반환, 이후 데이터 로딩 완료 되면 다시 렌더링
    
    ✔ fallback : true 흐름
    1. 미리 렌더링 안된 경로 접근 시
    2. getStaticProps가 백그라운드에서 실행됨
    3. router.isFallback이 true인 상태 -> "로딩중입니다" 등의 UI 렌더
    4. 데이터 로딩 완료 후, 실제 props가 적용된 페이지 렌더
    
    📌 Next.js는 이 경로를 정적 파일로 저장해 다음 접근부터는 SSG처럼 빠르게 처리함
    
    ✔ 예시 코드 흐름 정리
    - getStaticPaths
    export const getStaticPaths = () => ({
      paths: [
        { params: { id: "1" } },
        { params: { id: "2" } },
        { params: { id: "3" } },
      ],
      fallback: true, // 미리 생성되지 않은 페이지도 요청 가능하게 함
    });
    
    - getStaticProps
    export const getStaticProps = async (context) => {
      const id = context.params!.id;
      const book = await fetchOneBook(Number(id));
    
      if (!book) return { notFound: true }; // 데이터 없으면 404 처리
      return { props: { book } };
    };
    
    - 컴포넌트 내부 처리
    const router = useRouter();
    if (router.isFallback) return "로딩중입니다"; // fallback true일 때 초기 상태 처리
    
    if (!book) return "문제가 발생했습니다 다시 시도하세요"; // 예외 처리
    
    💥 언제 fallback: true를 쓸까?
    - 데이터가 자주 추가되는 페이지( 예 : 블로그, 상품 등)
    - 모든 경로를 미리 정의하기 어려운 경우
    - 초기 응답 속도를 빠르게 하고, 나중에 데이터 채워 넣는 UX를 원할 때

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -2,6 +2,8 @@ import { GetServerSidePropsContext, GetStaticPropsContext } from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-books";
 import { InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
+import { useRouter } from "next/router";
+import { notFound } from "next/navigation";
 
 const mockData = {
   id: 1,
@@ -24,7 +26,10 @@ export const getStaticPaths = () => {
       { params: { id: "3" } },
     ],
     // fallback : 대체, 대비책
-    fallback: false, // false : NotFound 없는 페이지
+    fallback: true,
+    // false : 404 Notfound
+    // blocking : SSR 방식
+    // true : SSR 방식 + 데이터가 없는 폴백 상태의 페이지부터 반환
   };
 };
 
@@ -32,6 +37,12 @@ export const getStaticPaths = () => {
 export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
+
+  if (!book) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: { book },
@@ -41,7 +52,12 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
 export default function Page({
   book,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  // 페이지 컴퍼넌트가 fallback 상태일 때
+  const router = useRouter();
+  if (router.isFallback) return "로딩중입니다";
+
   if (!book) return "문제가 발생했습니다 다시 시도하세요";
+
   const { id, title, subTitle, description, author, publisher, coverImgUrl } =
     book;
 
